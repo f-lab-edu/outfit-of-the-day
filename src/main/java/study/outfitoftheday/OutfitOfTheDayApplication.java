836cd5de086b4510e73fa7c7e212a52770c9de13
jongwanra <najongsi@gmail.com>
2023-10-12 14:27:42 +0900

    [none] chore: 프로젝트 셋팅

---
+++ src/main/java/study/outfitoftheday/OutfitOfTheDayApplication.java
@@ -0,0 +1,13 @@
+package study.outfitoftheday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutfitOfTheDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutfitOfTheDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/outfitoftheday/OutfitOfTheDayApplicationTests.java
@@ -0,0 +1,13 @@
+package study.outfitoftheday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutfitOfTheDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
