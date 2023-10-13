f1fd91489f2086a3c5e2d5a16f677f677173a44c
jongwanra <najongsi@gmail.com>
2023-10-13 15:15:47 +0900

    [#1] feature: SecurityConfig 및 PasswordEncoder 추가

---
+++ src/main/java/study/outfitoftheday/common/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package study.outfitoftheday.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class SecurityConfig {
+
+	@Value("${security.bcrypt.strength}")
+	private int strength;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(strength);
+	}
+}
