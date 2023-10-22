dae87ab9bd7d6228225dbbee57612e3a125b65de
jongwanra <najongsi@gmail.com>
2023-10-22 17:26:48 +0900

    [None] refactor: schema.sql 주석 제거

--- src/main/resources/data/schema-local.sql
+++ src/main/resources/data/schema-local.sql
@@ -1,5 +1,10 @@
-DROP TABLE if EXISTS member;
+DROP TABLE if EXISTS follow;
+DROP TABLE if EXISTS post_like;
+DROP TABLE if EXISTS comment_like;
+DROP TABLE if EXISTS comment;
+DROP TABLE if EXISTS post;
 DROP TABLE if EXISTS history;
+DROP TABLE if EXISTS member;
 
 CREATE TABLE `member`
 (
@@ -11,76 +16,77 @@ CREATE TABLE `member`
     `profile_image_url` varchar(255),
     `created_at`        timestamp          NOT NULL,
     `updated_at`        timestamp          NOT NULL,
-    `created_by`        bigint NULL,
-    `updated_by`        bigint NULL,
+    `created_by`        bigint             NOT NULL,
+    `updated_by`        bigint             NOT NULL,
     `is_deleted`        boolean            NOT NULL DEFAULT false
 );
 
--- CREATE TABLE `comment`
--- (
---     `comment_id`        bigint PRIMARY KEY,
---     `post_id`           bigint       NOT NULL,
---     `parent_comment_id` bigint       NOT NULL COMMENT '첫 번째 댓글인 경우 자기 자신, 대댓글인 경우 comment_id',
---     `writer_comment_id` bigint       NOT NULL,
---     `content`           text         NOT NULL,
---     `depth`             int          NOT NULL,
---     `created_at`        timestamp NOT NULL,
---     `updated_at`        timestamp NOT NULL,
---     `created_by`        bigint       NOT NULL,
---     `updated_by`        bigint       NOT NULL,
---     `is_deleted`        boolean      NOT NULL DEFAULT false
--- );
+CREATE TABLE `comment`
+(
+    `comment_id`        bigint PRIMARY KEY,
+    `post_id`           bigint    NOT NULL,
+    `parent_comment_id` bigint    NOT NULL COMMENT '첫 번째 댓글인 경우 자기 자신, 대댓글인 경우 comment_id',
+    `writer_comment_id` bigint    NOT NULL,
+    `content`           text      NOT NULL,
+    `depth`             int       NOT NULL,
+    `created_at`        timestamp NOT NULL,
+    `updated_at`        timestamp NOT NULL,
+    `created_by`        bigint    NOT NULL,
+    `updated_by`        bigint    NOT NULL,
+    `is_deleted`        boolean   NOT NULL DEFAULT false
+);
 
--- CREATE TABLE `post`
--- (
---     `post_id`           bigint PRIMARY KEY,
---     `writer_member_id`  bigint       NOT NULL,
---     `title`             varchar(255) NOT NULL,
---     `short_description` varchar(255) NOT NULL,
---     `post_status`       varchar(32)  NOT NULL,
---     `created_at`        timestamp NOT NULL,
---     `updated_at`        timestamp NOT NULL,
---     `created_by`        bigint       NOT NULL,
---     `updated_by`        bigint       NOT NULL,
---     `is_deleted`        boolean      NOT NULL DEFAULT false
--- );
---
--- CREATE TABLE `post_like`
--- (
---     `post_like_id` bigint PRIMARY KEY,
---     `member_id`    bigint       NOT NULL,
---     `post_id`      bigint       NOT NULL,
---     `created_at`   timestamp NOT NULL,
---     `updated_at`   timestamp NOT NULL,
---     `created_by`   bigint       NOT NULL,
---     `updated_by`   bigint       NOT NULL,
---     `is_deleted`   boolean      NOT NULL DEFAULT false
--- );
---
--- CREATE TABLE `comment_like`
--- (
---     `comment_like_id` bigint PRIMARY KEY,
---     `member_id`       bigint       NOT NULL,
---     `comment_id`      bigint       NOT NULL,
---     `created_at`      timestamp NOT NULL,
---     `updated_at`      timestamp NOT NULL,
---     `created_by`      bigint       NOT NULL,
---     `updated_by`      bigint       NOT NULL,
---     `is_deleted`      boolean      NOT NULL DEFAULT false
--- );
---
--- CREATE TABLE `follow`
--- (
---     `follow_id`      bigint PRIMARY KEY,
---     `from_member_id` bigint       NOT NULL,
---     `to_member_id`   bigint       NOT NULL,
---     `created_at`     timestamp NOT NULL,
---     `updated_at`     timestamp NOT NULL,
---     `created_by`     bigint       NOT NULL,
---     `updated_by`     bigint       NOT NULL,
---     `is_deleted`     boolean      NOT NULL DEFAULT false
--- );
---
+CREATE TABLE `post`
+(
+    `post_id`           bigint PRIMARY KEY,
+    `writer_member_id`  bigint       NOT NULL,
+    `title`             varchar(255) NOT NULL,
+    `short_description` varchar(255) NOT NULL,
+    `post_status`       varchar(32)  NOT NULL,
+    `created_at`        timestamp    NOT NULL,
+    `updated_at`        timestamp    NOT NULL,
+    `created_by`        bigint       NOT NULL,
+    `updated_by`        bigint       NOT NULL,
+    `is_deleted`        boolean      NOT NULL DEFAULT false
+);
+
+
+CREATE TABLE `post_like`
+(
+    `post_like_id` bigint PRIMARY KEY,
+    `member_id`    bigint    NOT NULL,
+    `post_id`      bigint    NOT NULL,
+    `created_at`   timestamp NOT NULL,
+    `updated_at`   timestamp NOT NULL,
+    `created_by`   bigint    NOT NULL,
+    `updated_by`   bigint    NOT NULL,
+    `is_deleted`   boolean   NOT NULL DEFAULT false
+);
+
+CREATE TABLE `comment_like`
+(
+    `comment_like_id` bigint PRIMARY KEY,
+    `member_id`       bigint    NOT NULL,
+    `comment_id`      bigint    NOT NULL,
+    `created_at`      timestamp NOT NULL,
+    `updated_at`      timestamp NOT NULL,
+    `created_by`      bigint    NOT NULL,
+    `updated_by`      bigint    NOT NULL,
+    `is_deleted`      boolean   NOT NULL DEFAULT false
+);
+
+CREATE TABLE `follow`
+(
+    `follow_id`      bigint PRIMARY KEY,
+    `from_member_id` bigint    NOT NULL,
+    `to_member_id`   bigint    NOT NULL,
+    `created_at`     timestamp NOT NULL,
+    `updated_at`     timestamp NOT NULL,
+    `created_by`     bigint    NOT NULL,
+    `updated_by`     bigint    NOT NULL,
+    `is_deleted`     boolean   NOT NULL DEFAULT false
+);
+
 CREATE TABLE `history`
 (
     `history_id`     bigint PRIMARY KEY,
@@ -90,30 +96,30 @@ CREATE TABLE `history`
     `created_at`     timestamp   NOT NULL,
     `created_by`     bigint      NOT NULL
 );
---
--- ALTER TABLE `comment`
---     ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
---
--- ALTER TABLE `post`
---     ADD FOREIGN KEY (`writer_member_id`) REFERENCES `member` (`member_id`);
---
--- ALTER TABLE `comment`
---     ADD FOREIGN KEY (`writer_comment_id`) REFERENCES `member` (`member_id`);
---
--- ALTER TABLE `post_like`
---     ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
---
--- ALTER TABLE `post_like`
---     ADD FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
---
--- ALTER TABLE `comment_like`
---     ADD FOREIGN KEY (`comment_id`) REFERENCES `comment` (`comment_id`);
---
--- ALTER TABLE `comment_like`
---     ADD FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
---
--- ALTER TABLE `follow`
---     ADD FOREIGN KEY (`from_member_id`) REFERENCES `member` (`member_id`);
---
--- ALTER TABLE `follow`
---     ADD FOREIGN KEY (`to_member_id`) REFERENCES `member` (`member_id`);
+
+ALTER TABLE `comment`
+    ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
+
+ALTER TABLE `post`
+    ADD FOREIGN KEY (`writer_member_id`) REFERENCES `member` (`member_id`);
+
+ALTER TABLE `comment`
+    ADD FOREIGN KEY (`writer_comment_id`) REFERENCES `member` (`member_id`);
+
+ALTER TABLE `post_like`
+    ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
+
+ALTER TABLE `post_like`
+    ADD FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
+
+ALTER TABLE `comment_like`
+    ADD FOREIGN KEY (`comment_id`) REFERENCES `comment` (`comment_id`);
+
+ALTER TABLE `comment_like`
+    ADD FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
+
+ALTER TABLE `follow`
+    ADD FOREIGN KEY (`from_member_id`) REFERENCES `member` (`member_id`);
+
+ALTER TABLE `follow`
+    ADD FOREIGN KEY (`to_member_id`) REFERENCES `member` (`member_id`);
