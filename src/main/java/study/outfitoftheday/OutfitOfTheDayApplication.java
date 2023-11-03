6b7b6bf849d99a111c75fb55f84b1ddf5c2b52dc
jongwanra <najongsi@gmail.com>
2023-11-03 22:46:09 +0900

    [#11] feature: logback 관련 초기 셋팅 작업 추가

--- src/main/java/study/outfitoftheday/OutfitOfTheDayApplication.java
+++ src/main/java/study/outfitoftheday/OutfitOfTheDayApplication.java
@@ -31,11 +31,12 @@ public class OutfitOfTheDayApplication {
 		 * Todo
 		 *  Logging 관련 Test진행 이후에 삭제 필요
 		 * */
+
+		log.trace("trace level test log");
+		log.debug("debug level test log");
 		log.info("info level test log");
 		log.warn("warn level test log");
 		log.error("error level test log");
-		log.debug("debug level test log");
-
 	}
 
 }
