804efa5a495befb50eda9806710eeaf35db54e5f
jongwanra <najongsi@gmail.com>
2023-11-10 17:05:00 +0900

    [#16] feat: Post validation check logic 추가

--- src/main/java/study/outfitoftheday/domain/post/entity/Post.java
+++ src/main/java/study/outfitoftheday/domain/post/entity/Post.java
@@ -16,12 +16,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import study.outfitoftheday.domain.member.entity.Member;
 import study.outfitoftheday.domain.post.enumurate.PostStatus;
+import study.outfitoftheday.domain.post.exception.InvalidPostException;
 import study.outfitoftheday.global.BaseEntity;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseEntity {
+	private final static int MAX_SHORT_DESCRIPTION_LENGTH = 255;
+	private final static int MAX_TITLE_LENGTH = 255;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,12 +47,53 @@ public class Post extends BaseEntity {
 	private Member member;
 
 	@Builder
-	public Post(Member member, String title, String shortDescription, String content) {
-		this.member = member;
+	private Post(String title, String shortDescription, String content, Member member, PostStatus postStatus) {
+		validateTitle(title);
+		validateShortDescription(shortDescription);
+		validateContent(content);
+
 		this.title = title;
 		this.shortDescription = shortDescription;
 		this.content = content;
-		this.postStatus = PostStatus.DRAFT;
+		this.member = member;
+		this.postStatus = postStatus;
 		this.isDeleted = false;
+	}
+
+	private void validateContent(String content) {
+		if (content == null || content.isBlank()) {
+			throw new InvalidPostException("내용을 입력해 주시길 바랍니다.");
+		}
+	}
+
+	private void validateShortDescription(String shortDescription) {
+		if (shortDescription == null || shortDescription.isBlank()) {
+			throw new InvalidPostException("소제목은 필수값 입니다.");
+		}
+		if (shortDescription.length() > MAX_SHORT_DESCRIPTION_LENGTH) {
+			throw new InvalidPostException("최대 길이는 255자 입니다.");
+		}
+	}
+
+	private void validateTitle(String title) {
+		if (title == null || title.isBlank()) {
+			throw new InvalidPostException("제목은 필수값 입니다.");
+		}
+
+		if (title.length() > MAX_TITLE_LENGTH) {
+			throw new InvalidPostException("최대 길이는 255자 입니다.");
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "Post{" +
+			"id=" + id +
+			", title=" + title +
+			", shortDescription='" + shortDescription + '\'' +
+			", content='" + content + '\'' +
+			", postStatus=" + postStatus +
+			'}';
 	}
 }
