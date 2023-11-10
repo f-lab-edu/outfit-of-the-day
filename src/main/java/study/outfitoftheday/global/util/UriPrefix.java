d5ef67da8ac268f9cf477e57834d45410e026acc
jongwanra <najongsi@gmail.com>
2023-11-10 17:03:53 +0900

    [#16] feat: URIPrefix 추가

--- src/main/java/study/outfitoftheday/global/util/UriPrefix.java
+++ src/main/java/study/outfitoftheday/global/util/UriPrefix.java
@@ -3,4 +3,5 @@ package study.outfitoftheday.global.util;
 public final class UriPrefix {
 	public static final String AUTH_URI_PREFIX = "/api/auth";
 	public static final String MEMBER_URI_PREFIX = "/api/members";
+	public static final String POST_URI_PREFIX = "/api/posts";
 }
