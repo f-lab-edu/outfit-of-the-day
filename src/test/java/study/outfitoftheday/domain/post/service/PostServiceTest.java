ca81cb42f3f1a38c2a29a215cb5cd4ec520aebf3
jongwanra <najongsi@gmail.com>
2023-11-12 21:15:31 +0900

    [#16] test: PostService update test code추가

--- src/test/java/study/outfitoftheday/domain/post/service/PostServiceTest.java
+++ src/test/java/study/outfitoftheday/domain/post/service/PostServiceTest.java
@@ -17,12 +17,13 @@ import study.outfitoftheday.domain.member.service.MemberService;
 import study.outfitoftheday.domain.post.entity.Post;
 import study.outfitoftheday.domain.post.enumurate.PostStatus;
 import study.outfitoftheday.domain.post.exception.InvalidPostException;
-import study.outfitoftheday.domain.post.exception.NoAuthorizationToDeletePostException;
+import study.outfitoftheday.domain.post.exception.NoAuthorizationToAccessPostException;
 import study.outfitoftheday.domain.post.exception.NotFoundPostException;
 import study.outfitoftheday.domain.post.repository.PostRepository;
 import study.outfitoftheday.web.auth.controller.request.AuthLoginRequest;
 import study.outfitoftheday.web.member.controller.request.MemberSignUpRequest;
 import study.outfitoftheday.web.post.controller.request.PostCreateRequest;
+import study.outfitoftheday.web.post.controller.request.PostUpdateRequest;
 
 @SpringBootTest
 class PostServiceTest {
@@ -134,7 +135,7 @@ class PostServiceTest {
 	}
 	
 	@Test
-	@DisplayName("게시글 등록 시, 요약글을 작성하지 않아서 예외가 발생한다.")
+	@DisplayName("게시글 등록 시, 소제목을 작성하지 않아서 예외가 발생한다.")
 	void create4() {
 		// given
 		memberService.signUp(createMemberSignUpRequest());
@@ -157,31 +158,8 @@ class PostServiceTest {
 	}
 	
 	@Test
-	@DisplayName("게시글 등록 시, 요약글을 작성하지 않아서 예외가 발생한다.")
-	void create5() {
-		// given
-		memberService.signUp(createMemberSignUpRequest());
-		authService.login(AuthLoginRequest.builder().loginId(LOGIN_ID).password(PASSWORD).build());
-		Member foundMember = memberRepository.findByLoginIdAndIsDeletedIsFalse(LOGIN_ID).orElseThrow();
-		final String emptyShortDescription = "";
-		
-		PostCreateRequest request = PostCreateRequest
-			.builder()
-			.title(TITLE)
-			.shortDescription(emptyShortDescription)
-			.postStatus(PostStatus.PUBLIC)
-			.content(CONTENT)
-			.build();
-		
-		// when & then
-		assertThatThrownBy(() -> postService.create(foundMember, request))
-			.hasMessage("소제목은 필수값 입니다.")
-			.isInstanceOf(InvalidPostException.class);
-	}
-	
-	@Test
 	@DisplayName("게시글 등록 시, 본문을 작성하지 않아서 예외가 발생한다.")
-	void create6() {
+	void create5() {
 		// given
 		memberService.signUp(createMemberSignUpRequest());
 		authService.login(AuthLoginRequest.builder().loginId(LOGIN_ID).password(PASSWORD).build());
@@ -284,8 +262,133 @@ class PostServiceTest {
 		
 		// when & then
 		assertThatThrownBy(() -> postService.delete(memberB, memberAPostId))
-			.isInstanceOf(NoAuthorizationToDeletePostException.class)
+			.isInstanceOf(NoAuthorizationToAccessPostException.class)
 			.hasMessage("해당 게시글을 삭제할 권한이 없습니다.");
+	}
+	
+	@DisplayName("게시글의 제목, 요약글, 본문, 상태를 성공적으로 변경한다.")
+	@Test
+	void update() {
+		// given
+		memberService.signUp(createMemberSignUpRequest(LOGIN_ID, NICKNAME, PASSWORD));
+		authService.login(AuthLoginRequest.builder().loginId(LOGIN_ID).password(PASSWORD).build());
+		Member loginMember = memberRepository.findByLoginIdAndIsDeletedIsFalse(LOGIN_ID).orElseThrow();
+		
+		PostCreateRequest postCreateRequest = PostCreateRequest
+			.builder()
+			.title(TITLE)
+			.shortDescription(SHORT_DESCRIPTION)
+			.postStatus(PostStatus.PUBLIC)
+			.content(CONTENT)
+			.build();
+		
+		final Long postId = postService.create(loginMember, postCreateRequest);
+		
+		final String titleToUpdate = RandomString.make(TITLE_MAX_LENGTH);
+		final String contentToUpdate = RandomString.make();
+		final String shortDescriptionToUpdate = RandomString.make(SHORT_DESCRIPTION_MAX_LENGTH);
+		final PostStatus statusToUpdate = PostStatus.PRIVATE;
+		
+		PostUpdateRequest postUpdateRequest = PostUpdateRequest.builder()
+			.postId(postId)
+			.shortDescription(shortDescriptionToUpdate)
+			.postStatus(statusToUpdate)
+			.title(titleToUpdate)
+			.content(contentToUpdate)
+			.build();
+		
+		// when
+		postService.update(loginMember, postUpdateRequest);
+		Post updatedPost = postService.findById(postId);
+		
+		// then
+		assertThat(updatedPost.getId()).isEqualTo(postId);
+		assertThat(updatedPost.getContent()).isEqualTo(contentToUpdate);
+		assertThat(updatedPost.getShortDescription()).isEqualTo(shortDescriptionToUpdate);
+		assertThat(updatedPost.getTitle()).isEqualTo(titleToUpdate);
+		assertThat(updatedPost.getPostStatus()).isEqualTo(statusToUpdate);
+		assertThat(updatedPost.getIsDeleted()).isFalse();
+	}
+	
+	@DisplayName("memberA가 작성한 게시글을 memberB가 변경하려고 할 경우 예외를 발생시킨다.")
+	@Test
+	void update2() {
+		// given
+		
+		final String memberALoginId = LOGIN_ID;
+		final String memberAPassword = PASSWORD;
+		final String memberANickname = NICKNAME;
+		
+		memberService.signUp(createMemberSignUpRequest(memberALoginId, memberANickname, memberAPassword));
+		authService.login(AuthLoginRequest.builder().loginId(memberALoginId).password(memberAPassword).build());
+		Member memberA = memberRepository.findByLoginIdAndIsDeletedIsFalse(memberALoginId).orElseThrow();
+		
+		PostCreateRequest request = PostCreateRequest
+			.builder()
+			.title(TITLE)
+			.shortDescription(SHORT_DESCRIPTION)
+			.postStatus(PostStatus.PUBLIC)
+			.content(CONTENT)
+			.build();
+		
+		final Long memberAPostId = postService.create(memberA, request);
+		
+		authService.logout(); // memberA logout
+		
+		final String memberBLoginId = "member_b@naver.com";
+		final String memberBPassword = "mdoiqn123";
+		final String memberBNickname = "memberB";
+		
+		memberService.signUp(createMemberSignUpRequest(memberBLoginId, memberBNickname, memberBPassword));
+		authService.login(AuthLoginRequest.builder().loginId(memberBLoginId).password(memberBPassword).build());
+		Member memberB = memberRepository.findByLoginIdAndIsDeletedIsFalse(memberBLoginId).orElseThrow();
+		
+		final String titleToUpdate = RandomString.make(TITLE_MAX_LENGTH);
+		final String contentToUpdate = RandomString.make();
+		final String shortDescriptionToUpdate = RandomString.make(SHORT_DESCRIPTION_MAX_LENGTH);
+		final PostStatus statusToUpdate = PostStatus.PRIVATE;
+		
+		PostUpdateRequest postUpdateRequest = PostUpdateRequest.builder()
+			.postId(memberAPostId)
+			.shortDescription(shortDescriptionToUpdate)
+			.postStatus(statusToUpdate)
+			.title(titleToUpdate)
+			.content(contentToUpdate)
+			.build();
+		
+		// when & then
+		assertThatThrownBy(() -> postService.update(memberB, postUpdateRequest))
+			.isInstanceOf(NoAuthorizationToAccessPostException.class)
+			.hasMessage("해당 게시글을 변경할 권한이 없습니다.");
+	}
+	
+	@DisplayName("존재하지 않는 게시글을 변경하려고 할 경우 예외를 발생시킨다.")
+	@Test
+	void update3() {
+		// given
+		
+		memberService.signUp(createMemberSignUpRequest(LOGIN_ID, NICKNAME, PASSWORD));
+		authService.login(AuthLoginRequest.builder().loginId(LOGIN_ID).password(PASSWORD).build());
+		Member member = memberRepository.findByLoginIdAndIsDeletedIsFalse(LOGIN_ID).orElseThrow();
+		
+		final Long notExistPostId = 2023L;
+		final String titleToUpdate = RandomString.make(TITLE_MAX_LENGTH);
+		final String contentToUpdate = RandomString.make();
+		final String shortDescriptionToUpdate = RandomString.make(SHORT_DESCRIPTION_MAX_LENGTH);
+		final PostStatus statusToUpdate = PostStatus.PRIVATE;
+		
+		PostUpdateRequest postUpdateRequest = PostUpdateRequest.builder()
+			.postId(notExistPostId)
+			.shortDescription(shortDescriptionToUpdate)
+			.postStatus(statusToUpdate)
+			.title(titleToUpdate)
+			.content(contentToUpdate)
+			.build();
+		
+		// when & then
+		assertThatThrownBy(() -> postService.update(member, postUpdateRequest))
+			.isInstanceOf(NotFoundPostException.class)
+			.hasMessage("변경할 게시글이 존재하지 않습니다.");
 	}
 	
 	private MemberSignUpRequest createMemberSignUpRequest() {
